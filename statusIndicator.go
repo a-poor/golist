19873009cc8f440d094dbf11f8f56236d9b2e36d
a-poor <45295232+a-poor@users.noreply.github.com>
2021-10-14 16:12:57 -0700

    Converting StatusIndicator
    Moving from a closure function to an interface. Adding a static
    and a cycling indicator.
    
    Creating a type to map `TaskStatus` to the corresponding indicator.
    
    Adding a default indicator.

--- statusIndicator.go
+++ statusIndicator.go
@@ -1,20 +1,135 @@
 package golist
 
-func toBlack(s string) string {
+// ToBlack wraps a string in escape characters
+// to format it as black text.
+func ToBlack(s string) string {
 	return "\033[30;1m" + s + "\033[0m"
 }
 
-func toYellow(s string) string {
+// ToYellow wraps a string in escape characters
+// to format it as yellow text.
+func ToYellow(s string) string {
 	return "\033[33;1m" + s + "\033[0m"
 }
 
-func toGreen(s string) string {
+// ToGreen wraps a string in escape characters
+// to format it as green text.
+func ToGreen(s string) string {
 	return "\033[32;1m" + s + "\033[0m"
 }
 
-func toRed(s string) string {
+// ToRed wraps a string in escape characters
+// to format it as red text.
+func ToRed(s string) string {
 	return "\033[31;1m" + s + "\033[0m"
 }
+
+type Indicator interface {
+	Get() string // Get the current indicator character
+	Next()       // Move to the next indicator
+}
+
+type StaticIndicator struct {
+	Indicator rune                // Character to return
+	Colorizer func(string) string // Optional function to colorize the indicator
+}
+
+// Get returns the current status indicator.
+// If Colorizer is set, calls it on the indicator character.
+func (si *StaticIndicator) Get() string {
+	s := string(si.Indicator)
+	if si.Colorizer == nil {
+		return s
+	}
+	return si.Colorizer(s)
+}
+
+// Next is a no-op for StaticIndicator
+func (si *StaticIndicator) Next() {
+	// Do nothing
+}
+
+type CycleIndicator struct {
+	Indicators []rune              // Array of indicator characters
+	index      int                 // Current position in the Indicators array
+	Colorizer  func(string) string // Optional function to colorize the indicator
+}
+
+// Get returns the current status indicator.
+// If Colorizer is set, calls it on the indicator character.
+func (si *CycleIndicator) Get() string {
+	s := string(si.Indicators[si.index])
+	if si.Colorizer == nil {
+		return s
+	}
+	return si.Colorizer(s)
+}
+
+// Next increments the current index in the Indicators array
+// and wraps around if passed the end of the array.
+func (si *CycleIndicator) Next() {
+	si.index = (si.index + 1) % len(si.Indicators)
+}
+
+// StatusIndicators is a map from task statuses to an indicator.
+type StatusIndicators map[TaskStatus]Indicator
+
+// Get returns the current status indicator character
+// for the corresponding TaskStatus in the StatusIndicators map.
+//
+// Note: If the TaskStatus is not found in the StatusIndicators map,
+// the uncolorized string "–" is returned
+func (si *StatusIndicators) Get() string {
+	backup := "–"
+	s, ok := (*si)[TaskNotStarted]
+	if !ok {
+		return backup
+	}
+	return s.Get()
+}
+
+// Next calls Next on all indicators
+func (si *StatusIndicators) Next() {
+	for _, i := range *si {
+		i.Next()
+	}
+}
+
+// CreateDefaultStatusIndicator creates a StatusIndicators map
+// with default values for each status.
+//
+// The default values are:
+//   – TaskNotStarted: "➜"
+//   – TaskInProgress: "⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏" (yellow)
+//   – TaskCompleted: "✓" (green)
+//   – TaskFailed: "✗" (red)
+//   – TaskSkipped: "↓" (black)
+//
+func CreateDefaultStatusIndicator() StatusIndicators {
+	return StatusIndicators{
+		TaskNotStarted: &StaticIndicator{
+			Indicator: '➜',
+		},
+		TaskInProgress: &CycleIndicator{
+			Indicators: []rune("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"),
+			Colorizer:  ToYellow,
+		},
+		TaskCompleted: &StaticIndicator{
+			Indicator: '✓',
+			Colorizer: ToGreen,
+		},
+		TaskFailed: &StaticIndicator{
+			Indicator: '✗',
+			Colorizer: ToRed,
+		},
+		TaskSkipped: &StaticIndicator{
+			Indicator: '↓',
+			Colorizer: ToBlack,
+		},
+	}
+}
+
+//////// OLD FUNCTION ////////////
 
 type statusIndicatorConfig struct {
 	NotStarted string
@@ -70,13 +185,13 @@ func createStatusIndicator(config statusIndicatorConfig) func(TaskStatus) string
 		var colorFn func(string) string
 		switch status {
 		case TaskNotStarted, TaskSkipped:
-			colorFn = toBlack
+			colorFn = ToBlack
 		case TaskInProgress:
-			colorFn = toYellow
+			colorFn = ToYellow
 		case TaskCompleted:
-			colorFn = toGreen
+			colorFn = ToGreen
 		case TaskFailed:
-			colorFn = toRed
+			colorFn = ToRed
 		default:
 			colorFn = func(s string) string {
 				return s
